06bdbfd020b481990d3ff47a8c27324d3872adfb
Brayden Killeen <braydenmkilleen@gmail.com>
2019-09-18 22:19:36 +1000

    Added eslintignore & updated prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
   extends: ['airbnb', 'prettier'],
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': ['error']
-  }
+    'prettier/prettier': ['error'],
+  },
 };
