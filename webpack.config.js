238a60713189280e5dc84539187775621e3e80a0
Brayden Killeen <braydenmkilleen@gmail.com>
2019-09-18 21:57:06 +1000

    Added prod & dev env build

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,38 @@
-const path = require('path')
+const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
-      },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-    ]
-  }
-}
+module.exports = (env = {}) => {
+  const isProduction = env.production === true;
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: {
+      main: ['./src/index.js']
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: './dist',
+      hot: true
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ['file-loader']
+        }
+      ]
+    }
+  };
+};
