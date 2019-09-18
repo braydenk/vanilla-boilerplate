b5e1488760a1e8ed338ccd936c7d13ba620f5008
Brayden Killeen <braydenmkilleen@gmail.com>
2019-09-18 22:45:27 +1000

    Merge config based on production env variable

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -2,47 +2,64 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+const baseConfig = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ['file-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Boilerplate',
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, '../', 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+};
+
+const devConfig = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    open: true,
+  },
+};
+
+const productionConfig = {
+  mode: 'production',
+  devtool: 'source-map',
+};
+
+function getEnvConfig(isProduction) {
+  return isProduction ? productionConfig : devConfig;
+}
+
 module.exports = ({ env }) => {
-  const isProduction = env.production === true;
+  const isProduction = env.production;
+  const envConfig = getEnvConfig(isProduction);
 
   return {
-    mode: isProduction ? 'production' : 'development',
-    entry: {
-      main: ['./src/index.js']
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist',
-      hot: true
-    },
-    output: {
-      path: path.resolve(__dirname, '../', 'dist'),
-      publicPath: '/',
-      filename: 'bundle.js'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: ['file-loader']
-        }
-      ]
-    },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        title: 'Boilerplate',
-        template: './src/index.html'
-      })
-    ]
+    ...baseConfig,
+    ...envConfig,
   };
 };
